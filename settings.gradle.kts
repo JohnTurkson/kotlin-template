7797f58e424da5b850f23504067029fc12a8d2c0
John Turkson <git@johnturkson.com>
2022-12-29 22:41:11 -0800

    Update credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ pluginManagement {
         maven {
             url = uri("https://maven.pkg.github.com/JohnTurkson/packages")
             credentials {
-                val githubActor: String? by settings
+                val githubUsername: String? by settings
                 val githubToken: String? by settings
-                username = githubActor ?: System.getenv("GITHUB_ACTOR")
+                username = githubUsername ?: System.getenv("GITHUB_USERNAME")
                 password = githubToken ?: System.getenv("GITHUB_TOKEN")
             }
         }
@@ -31,9 +31,9 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/JohnTurkson/packages")
             credentials {
-                val githubActor: String? by settings
+                val githubUsername: String? by settings
                 val githubToken: String? by settings
-                username = githubActor ?: System.getenv("GITHUB_ACTOR")
+                username = githubUsername ?: System.getenv("GITHUB_USERNAME")
                 password = githubToken ?: System.getenv("GITHUB_TOKEN")
             }
         }
