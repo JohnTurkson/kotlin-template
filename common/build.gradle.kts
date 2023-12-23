e5ffe41502cb1206a72a885db3930c9beb34fa65
John Turkson <github@johnturkson.com>
2023-12-22 23:01:15 -0800

    Add kotlinx.serialization

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
@@ -8,6 +9,15 @@ kotlin {
     
     jvm()
     androidTarget()
+    
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project.dependencies.platform(libs.kotlinx.serialization.bom))
+                implementation(libs.kotlinx.serialization)
+            }
+        }
+    }
 }
 
 android {
