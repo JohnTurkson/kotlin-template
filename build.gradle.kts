f228fe05cc9e67e66c82605a28d25f511b2c0d1d
John Turkson <github@johnturkson.com>
2023-12-22 21:59:43 -0800

    Add ksp plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.ksp) apply false
 }
 
 group = "com.johnturkson.kotlin.template"
